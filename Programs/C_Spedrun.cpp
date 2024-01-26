31514e6ffa6f890a3e0b7032de57df3dcaf47165
JothishKamal <jothishkamal@gmail.com>
2024-01-26 20:52:44 +0530

    edit: C_Spedrun.cpp

--- Programs/C_Spedrun.cpp
+++ Programs/C_Spedrun.cpp
@@ -1,4 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(void){
+  int n;
+  cin>>n;
+  for(int i=0, x=1; i<n; i++){
+    for(int j=0; j<n-i-1; j++){
+      cout << " ";
+    }
+    for(int j=0; j<=i; j++){
+      cout << x++ << " ";
+    }
+    cout << endl;
+  }
 }
