cb7dcf20d95cbea4bf9974fd80c89798ceb3e953
JothishKamal <jothishkamal@gmail.com>
2024-03-09 09:19:55 +0530

    vscode: backup 09.19am

--- Programs/Cpp_Programs.cpp
+++ Programs/Cpp_Programs.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include<stdio.h>
 using namespace std;
 
 // // Types of Constructors
@@ -36,69 +37,82 @@ using namespace std;
 // }
 
 // Q. Object arr with deep copy
-class arr{
-  int *a,size;
-  public:
-    arr(){}
+// class arr{
+//   int *a,size;
+//   public:
+//     arr(){}
 
-    arr(int *a, int n){
-      this->size = n;
-      this->a = new int(n);
-      for(int i=0; i<n; i++){
-        this->a[i]=a[i];
-      }
-    }
+//     arr(int *a, int n){
+//       this->size = n;
+//       this->a = new int(n);
+//       for(int i=0; i<n; i++){
+//         this->a[i]=a[i];
+//       }
+//     }
 
-    arr(arr &a){
-      this->size = a.size;
-      this->a = new int(a.size);
-      for(int i=0; i<a.size; i++){
-        this->a[i]=a.a[i];
-      }
-    }
+//     arr(arr &a){
+//       this->size = a.size;
+//       this->a = new int(a.size);
+//       for(int i=0; i<a.size; i++){
+//         this->a[i]=a.a[i];
+//       }
+//     }
 
-    void updArr(){
-      int size=0;
-      for(int i=0; i<this->size; i++){
-        if(this->a[i]%2!=0){
-          size++;
-        }
-      }
+//     void updArr(){
+//       int size=0;
+//       for(int i=0; i<this->size; i++){
+//         if(this->a[i]%2!=0){
+//           size++;
+//         }
+//       }
 
-      int *x=new int(size);
+//       int *x=new int(size);
       
-      for(int i=0,tmp=0; i<this->size; i++){
-        if(this->a[i]%2!=0){
-          x[tmp++]=this->a[i];
-        }
-      }
+//       for(int i=0,tmp=0; i<this->size; i++){
+//         if(this->a[i]%2!=0){
+//           x[tmp++]=this->a[i];
+//         }
+//       }
 
-      a=x;
-      this->size=size;
-    }
+//       a=x;
+//       this->size=size;
+//     }
 
-    void putArr(){
-      for(int i=0; i<this->size; i++){
-        cout<<this->a[i]<<" ";
-      }
-    }
+//     void putArr(){
+//       for(int i=0; i<this->size; i++){
+//         cout<<this->a[i]<<" ";
+//       }
+//     }
 
-    ~arr(){
-      delete a;
-    }
-};
+//     ~arr(){
+//       delete a;
+//     }
+// };
 
-int main(void){
-  int n;
-  scanf("%d",&n);
-  int x[n];
-  for(int i=0; i<n; i++){
-    scanf("%d",&x[i]);
-  }
+// int main(void){
+//   int n;
+//   scanf("%d",&n);
+//   int x[n];
+//   for(int i=0; i<n; i++){
+//     scanf("%d",&x[i]);
+//   }
 
-  arr y(x,n);
-  y.updArr();
-  y.putArr();
+//   arr y(x,n);
+//   y.updArr();
+//   y.putArr();
 
-  cout<<endl;
-}
+//   cout<<endl;
+// }
+
+// // Pre-processor directives
+// #define PI 3.14
+// #define add(x,y) (x+y) // Macro
+// int main(void){
+//   float radius;
+//   scanf("%f",&radius);
+//   float area=PI*radius*radius;
+//   int a=6,b=9;
+//   int z=add(a,b);
+//   printf("%.2f %d",area,z);
+// }
+
